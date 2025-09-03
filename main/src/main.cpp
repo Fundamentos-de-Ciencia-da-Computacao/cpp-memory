7b014d57bdd65a591809ae74e1ef5ffa37121251
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2025-09-03 17:27:28 +0000

    memória de pilha e ponteiros

---
+++ main/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int num = 7;
+    std::cout << " num: " << num << std::endl;
+    std::cout << "&num: " << &num << std::endl;
+    std::cout << " " << std::endl;
+
+    int *p = &num;
+    std::cout << " p: " << p << std::endl;
+    std::cout << "&p: " << &p << std::endl;
+    std::cout << "*p: " << *p << std::endl;
+    std::cout << " " << std::endl;
+
+    *p = 42;
+    std::cout << "*p mudou para 42" << std::endl;
+    std::cout << "num: " << num << std::endl;
+    std::cout << " " << std::endl;
+
+    return 0;
+}
